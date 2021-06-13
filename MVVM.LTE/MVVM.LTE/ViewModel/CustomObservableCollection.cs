21ede3638b1be6d4840ff77442c41415833b5140
John Ansa <john.ansa@cyphercrescent.com>
2021-06-13 17:17:58 +0100

    added support for ObservableCollection. constructors implementation

--- MVVM.LTE/MVVM.LTE/ViewModel/CustomObservableCollection.cs
+++ MVVM.LTE/MVVM.LTE/ViewModel/CustomObservableCollection.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 
 namespace MVVM.LTE.ViewModel
 {
     public class CustomObservableCollection<T> : ObservableCollection<T>
     {
-
+        public CustomObservableCollection(IEnumerable<T> collection):base(collection) {  }
+        public CustomObservableCollection(List<T> list) :base(list) {  }
         public void Refresh(T item)
         {
             var index = IndexOf(item);
